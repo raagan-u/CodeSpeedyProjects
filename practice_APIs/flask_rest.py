7ee1ae72c6d83b9f964d11db375db23d268cf4fd
ru-root <n0r313n7@protonmail.ch>
2021-01-25 20:34:54 +0530

    added new route

--- practice_APIs/flask_rest.py
+++ practice_APIs/flask_rest.py
@@ -6,4 +6,7 @@ app = Flask(__name__)
 def root_handler():
 	return jsonify({ 'name': 'unknown' })
 
+@app.route("/new", methods = ['GET'])
+def new_dir():
+	return jsonify({ 'test': 'testing'})
 app.run(debug=True, host='0.0.0.0')
