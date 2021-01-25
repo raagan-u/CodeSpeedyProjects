5234860a4a13c0640cad5ccf423c94db5f04a45b
ru-root <n0r313n7@protonmail.ch>
2021-01-25 22:37:47 +0530

    started db services

--- practice_APIs/flask_rest.py
+++ practice_APIs/flask_rest.py
@@ -1,6 +1,18 @@
+import os
 from flask import Flask, jsonify
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_rest.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+se = Marshmallow(app)
+db = SQLAlchemy(app)
+
+
 
 @app.route("/", methods = ['GET'])
 def root_handler():
